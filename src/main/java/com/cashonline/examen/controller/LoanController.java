f34ed6bdffdd39ca976944c988b284b1e369e9d9
JuanPrato <jprato@fi.uba.ar>
2021-06-24 09:56:42 -0300

    Loan Controller tests, logger con loan controller

--- src/main/java/com/cashonline/examen/controller/LoanController.java
+++ src/main/java/com/cashonline/examen/controller/LoanController.java
@@ -4,6 +4,8 @@ import com.cashonline.examen.common.ResponseError;
 import com.cashonline.examen.common.loan.LoanPage;
 import com.cashonline.examen.exception.BadRequestException;
 import com.cashonline.examen.service.LoanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class LoanController {
 
     private final LoanService loanService;
+    private final Logger logger = LoggerFactory.getLogger(LoanController.class);
 
     @Autowired
     public LoanController(LoanService loanService) {
@@ -28,8 +31,10 @@ public class LoanController {
         try {
             response = this.loanService.getLoansPage(userId, page, size);
         } catch (BadRequestException ex) {
+            logger.error(String.format("BAD REQUEST EXCEPTION WITH id: %s, page: %s and size: %s => ", userId, page, size), ex);
             return ResponseEntity.badRequest().body(new ResponseError(ex));
         } catch (Exception ex) {
+            logger.error(String.format("UNKNOWN EXCEPTION WITH id: %s, page: %s and size: %s => ", userId, page, size), ex);
             return ResponseEntity.internalServerError().body(new ResponseError(ex));
         }
 
--- src/test/java/com/cashonline/examen/controller/LoanControllerTest.java
+++ src/test/java/com/cashonline/examen/controller/LoanControllerTest.java
@@ -1,4 +1,109 @@
 package com.cashonline.examen.controller;
 
+import com.cashonline.examen.common.ResponseError;
+import com.cashonline.examen.common.loan.LoanPage;
+import com.cashonline.examen.common.loan.PagingData;
+import com.cashonline.examen.dto.LoanDTO;
+import com.cashonline.examen.exception.BadRequestException;
+import com.cashonline.examen.model.Loan;
+import com.cashonline.examen.repository.LoanRepository;
+import com.cashonline.examen.repository.UserRepository;
+import com.cashonline.examen.service.LoanService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = LoanController.class)
 public class LoanControllerTest {
+
+    private LoanDTO loanDTO;
+    private Loan loan;
+    private LoanPage loanPage;
+    private ResponseError error;
+
+    private final String baseUri = "/loans";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private LoanService loanService;
+
+    @BeforeEach
+    public void setUp() {
+
+        loanDTO = new LoanDTO(1L, 1000.00F, 1L);
+        loan = new Loan(1L, 1000.00F, 1L);
+        loanPage = new LoanPage(Collections.singletonList(loanDTO), new PagingData(1, 1, 1));
+    }
+
+    @Test
+    public void testGetLoansOk () throws Exception {
+
+        Mockito.when(loanService.getLoansPage(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(loanPage);
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get(this.baseUri + "?page=1&size=1")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .accept(MediaType.APPLICATION_JSON))
+                                        .andExpect(status().isOk())
+                                        .andReturn();
+
+        Assertions.assertEquals(objectMapper.writeValueAsString(loanPage), result.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void testGetLoansBadRequest () throws Exception {
+
+        Mockito.when(loanService.getLoansPage(Mockito.any(), Mockito.any(), Mockito.any()))
+                    .thenThrow(new BadRequestException("Invalid page or size"));
+
+        error = new ResponseError(new BadRequestException("Invalid page or size"));
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get(this.baseUri + "?page=1&size1")
+                                        .accept(MediaType.APPLICATION_JSON))
+                                        .andExpect(status().isBadRequest())
+                                        .andReturn();
+
+
+        Assertions.assertEquals(objectMapper.writeValueAsString(error), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void testGetLoansInternalServerError () throws Exception {
+
+        Mockito.when(loanService.getLoansPage(Mockito.any(), Mockito.any(), Mockito.any())).thenThrow(RuntimeException.class);
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get(this.baseUri)
+                                        .accept(MediaType.APPLICATION_JSON))
+                                        .andExpect(status().isInternalServerError())
+                                        .andReturn();
+
+    }
+
 }
